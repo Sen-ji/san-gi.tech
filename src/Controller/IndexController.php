e37aed1e7b5d5288c59e39cf50f4ae8d02895836
Sangi <sangi@san-gi.tech>
2021-03-11 20:21:38 +0100

    test script

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,4 +17,13 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+    /**
+     * @Route("/test", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
 }
