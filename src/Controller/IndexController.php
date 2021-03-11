72edec0463ed7a5d3a2f2232ea1512ca374d131c
Sangi <sangi@san-gi.tech>
2021-03-11 20:21:55 +0100

    test script

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -20,7 +20,7 @@ class IndexController extends AbstractController
     /**
      * @Route("/test", name="index")
      */
-    public function index(): Response
+    public function test(): Response
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
