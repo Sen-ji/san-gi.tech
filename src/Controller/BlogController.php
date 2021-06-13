c67d121bebe19afe77c22c3880ffae34ca437a46
Damien Deturck <deturckdamien@gmail.com>
2021-06-13 18:41:22 +0200

    'readme'

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blogCode', name: 'blogCode')]
+    public function blogCode(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+    #[Route('/blogRando', name: 'blogRando')]
+    public function blogRando(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
