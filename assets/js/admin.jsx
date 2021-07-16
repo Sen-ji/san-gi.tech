8d4caadd484a666a9625e62ec6d852972041ad5a
Damien Deturck <deturckdamien@gmail.com>
2021-07-16 18:19:56 +0200

    admin

--- assets/js/admin.jsx
+++ assets/js/admin.jsx
@@ -4,24 +4,39 @@ import ReactDOM from 'react-dom';
 import  {Admin, Resource, ListGuesser} from  'react-admin';
 import restProvider from 'ra-data-simple-rest';
 import jsonServerProvider from "ra-data-json-server";
-import {
-    AdminGuesser,
-    hydraDataProvider,
-    hydraSchemaAnalyzer,
-} from '@api-platform/admin';
+
 import FieldGuesser from "@api-platform/admin/lib/FieldGuesser";
 import ResourceGuesser from "@api-platform/admin/lib/ResourceGuesser";
-import { HydraAdmin } from "@api-platform/admin";
-import { CategoryList,CategoryCreate,CategoryEdit,ArticlesList,ArticlesCreate,ArticlesEdit,CommentairesList,CommentairesCreate,CommentairesEdit,
-    TestList,TestCreate,TestEdit} from './posts';
 
+import { CategorieList,CategoryCreate,CategoryEdit,ArticlesList,ArticlesCreate,ArticlesEdit,CommentairesList,CommentairesCreate,CommentairesEdit,
+    TestList,TestCreate,TestEdit,UserList} from './posts';
+import { HydraAdmin, fetchHydra, hydraDataProvider } from "@api-platform/admin";
+import { parseHydraDocumentation } from "@api-platform/api-doc-parser";
+
+const entrypoint = 'https://127.0.0.1:8000/api';
+
+
+const dataProvider = hydraDataProvider(
+    entrypoint,
+    fetchHydra,
+    parseHydraDocumentation,
+    true // useEmbedded parameter
+);
 class Base extends React.Component{
     constructor(props) {
         super(props);
     }
     render(){
         return <div className=" ">
-                <HydraAdmin entrypoint='https://127.0.0.1:8000/api' />
+                <HydraAdmin
+                    entrypoint={ entrypoint }>
+                    <ResourceGuesser name={"tests"} list={TestList} />
+                    <ResourceGuesser name={"articles"} list={ArticlesList} />
+                    <ResourceGuesser name={"categories"} list={CategorieList} />
+                    <ResourceGuesser name={"commentaires"} list={CommentairesList} />
+                    <ResourceGuesser name={"users"} list={UserList} />
+                </HydraAdmin>
+
             </div>
 
     }
--- assets/js/posts.jsx
+++ assets/js/posts.jsx
@@ -5,141 +5,147 @@ import ListGuesser from "@api-platform/admin/lib/ListGuesser";
 import FieldGuesser from "@api-platform/admin/lib/FieldGuesser";
 export const PostIcon = BookIcon;
 
-export const CategoryList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <TextField source="type" />
-            <TextField source="name" />
-            <TextField source="enfants" />
-            <TextField source="articles" />
-        </Datagrid>
-    </List>
+export const CategorieList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"type"} />
+        <FieldGuesser source={"name"} />
+        <FieldGuesser source={"Parent"} />
+        <FieldGuesser source={"enfants"} />
+        <FieldGuesser source={"articles"} />
+        <FieldGuesser source={"parent"} />
+    </ListGuesser>
 );
 const PostTitle = ({ record }) => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
 };
 
-export const CategoryEdit = (props) => (
-    <Edit title={<PostTitle />} {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Edit>
-);
-
-export const CategoryCreate = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Create>
-);
+// export const CategoryEdit = (props) => (
+//     <Edit title={<PostTitle />} {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Edit>
+// );
+//
+// export const CategoryCreate = (props) => (
+//     <Create title="Create a Post" {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Create>
+// );
 
 
 export const ArticlesList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source={"name"} />
-            <TextField source={"author"} />
-            <TextField source={"PublicationDate"} />
-            <TextField source={"editDate"} />
-            <TextField source={"catégorie"} />
-            <TextField source={"commentaires"} />
-        </Datagrid>
-    </List>
+    <ListGuesser {...props}>
+        <FieldGuesser source={"name"} />
+        <FieldGuesser source={"author"} />
+        <FieldGuesser source={"PublicationDate"} />
+        <FieldGuesser source={"editDate"} />
+        <FieldGuesser source={"catégorie"} />
+        <FieldGuesser source={"commentaires"} />
+        <FieldGuesser source={"publicationDate"} />
+    </ListGuesser>
 );
 
-export const ArticlesEdit = (props) => (
-    <Edit title={<PostTitle />} {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Edit>
-);
 
-export const ArticlesCreate = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Create>
-);
+// export const ArticlesEdit = (props) => (
+//     <Edit title={<PostTitle />} {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Edit>
+// );
+//
+// export const ArticlesCreate = (props) => (
+//     <Create title="Create a Post" {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Create>
+// );
 export const CommentairesList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source={"name"} />
-            <TextField source={"author"} />
-            <TextField source={"PublicationDate"} />
-            <TextField source={"editDate"} />
-            <TextField source={"catégorie"} />
-            <TextField source={"commentaires"} />
-        </Datagrid>
-    </List>
+    <ListGuesser {...props}>
+        <FieldGuesser source={"author"} />
+        <FieldGuesser source={"publicationDate"} />
+        <FieldGuesser source={"editDate"} />
+        <FieldGuesser source={"article"} />
+    </ListGuesser>
 );
 
-export const CommentairesEdit = (props) => (
-    <Edit title={<PostTitle />} {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Edit>
-);
-
-export const CommentairesCreate = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-            <TextInput source="id" />
-            <TextInput source="type" />
-            <TextInput source="name" />
-            <TextInput source="enfants" />
-            <TextInput source="articles" />
-        </SimpleForm>
-    </Create>
-);
+// export const CommentairesEdit = (props) => (
+//     <Edit title={<PostTitle />} {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Edit>
+// );
+//
+// export const CommentairesCreate = (props) => (
+//     <Create title="Create a Post" {...props}>
+//         <SimpleForm>
+//             <TextInput source="id" />
+//             <TextInput source="type" />
+//             <TextInput source="name" />
+//             <TextInput source="enfants" />
+//             <TextInput source="articles" />
+//         </SimpleForm>
+//     </Create>
+// );
 export const TestList = props => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source={"name"} />
-        </Datagrid>
-    </List>
+    <ListGuesser {...props}>
+        <FieldGuesser source={"name"} />
+    </ListGuesser>
 );
 
-export const TestCreate = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-        </SimpleForm>
-    </Create>
-);
+// export const TestCreate = (props) => (
+//     <Create title="Create a Post" {...props}>
+//         <SimpleForm>
+//             <TextInput source="name" />
+//         </SimpleForm>
+//     </Create>
+// );
+//
+// export const TestEdit = (props) => (
+//     <Create title="Create a Post" {...props}>
+//         <SimpleForm>
+//
+//             <TextInput disabled source="id" />
+//             <TextInput source="name" />
+//
+//         </SimpleForm>
+//     </Create>
+// );
 
-export const TestEdit = (props) => (
-    <Create title="Create a Post" {...props}>
-        <SimpleForm>
-
-            <TextInput disabled source="id" />
-            <TextInput source="name" />
-
-        </SimpleForm>
-    </Create>
+export const UserList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"email"} />
+        <FieldGuesser source={"roles"} />
+        <FieldGuesser source={"password"} />
+        <FieldGuesser source={"pseudo"} />
+        <FieldGuesser source={"articles"} />
+        <FieldGuesser source={"commentaires"} />
+        <FieldGuesser source={"username"} />
+        <FieldGuesser source={"salt"} />
+    </ListGuesser>
 );
