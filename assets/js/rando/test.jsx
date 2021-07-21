13e8eeb273ad620a6c7f56850680cb2e49e1c3db
Damien Deturck <deturckdamien@gmail.com>
2021-07-22 01:02:30 +0200

    for sitemap

--- assets/js/rando/test.jsx
+++ assets/js/rando/test.jsx
@@ -1,9 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-
-class XSearch extends HTMLElement {
-    connectedCallback() {
-        ReactDOM.render(<p>salut</p>, this);
-    }
-}
-customElements.define('x-search', XSearch);
+10:82aa
